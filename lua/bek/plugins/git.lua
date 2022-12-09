29f29c360a3d90dcb87fc9b7ae25e880e2905a15
Allayor <htecgf@gmail.com>
2022-12-09 13:59:23 +0500

    Git signs config

--- lua/bek/plugins/git.lua
+++ lua/bek/plugins/git.lua
@@ -1,7 +1,24 @@
 local status, git = pcall(require, 'git')
 
 if not status then return end
-require('gitsigns').setup {}
+require('gitsigns').setup {
+  signs = {
+    add          = { hl = 'GitSignsAdd', text = '│', numhl = 'GitSignsAddNr', linehl = 'GitSignsAddLn' },
+    change       = { hl = 'GitSignsChange', text = '│', numhl = 'GitSignsChangeNr', linehl = 'GitSignsChangeLn' },
+    delete       = { hl = 'GitSignsDelete', text = '_', numhl = 'GitSignsDeleteNr', linehl = 'GitSignsDeleteLn' },
+    topdelete    = { hl = 'GitSignsDelete', text = '‾', numhl = 'GitSignsDeleteNr', linehl = 'GitSignsDeleteLn' },
+    changedelete = { hl = 'GitSignsChange', text = '~', numhl = 'GitSignsChangeNr', linehl = 'GitSignsChangeLn' },
+    untracked    = { hl = 'GitSignsAdd', text = '┆', numhl = 'GitSignsAddNr', linehl = 'GitSignsAddLn' },
+  },
+  numhl = true,
+  current_line_blame = true, -- Toggle with `:Gitsigns toggle_current_line_blame`
+  current_line_blame_opts = {
+    virt_text = true,
+    virt_text_pos = 'eol', -- 'eol' | 'overlay' | 'right_align'
+    delay = 550,
+    ignore_whitespace = false,
+  }
+}
 git.setup {
   keymaps = {
     blame = '<leader>gb',
